a7647557679447415dcf531dd0e750b3b9e146ce
Alejandro Esquivel <yoquiale@gmail.com>
2023-08-23 01:45:44 +0200

    Media addition input design improvement

--- src/components/reactionForm/ReactionURLInput.jsx
+++ src/components/reactionForm/ReactionURLInput.jsx
@@ -7,17 +7,17 @@ const onMediaSubmit = (e) => {
 function ReactionUrlInput(){
 
   return (
-    <div className="flex flex-row justify-center w-full min-h-full gap-0 shadow-lg focus:shadow-slate-600 shadow-slate-300">
+    <div className="relative flex flex-row justify-center w-full min-h-full gap-0 shadow-lg 0shadow-slate-600 shadow-slate-300">
       <input type="url"
         name="mediaURL"
         id="mediaURL"
-        className="relative z-20 w-11/12 px-4 text-xl transition duration-150 border-none rounded-l-lg focus:outline-green-400 focus:outline-2 bg-slate-700 text-gray-50 peer" />
-      <button id="mediaSubmit" title="Submit media" className="relative z-10 flex items-center justify-center w-1/12 rounded-r bg-slate-700 peer-focus:outline-green-400 peer-focus:outline-2 "
+        className="relative w-full px-4 text-xl transition duration-150 border-none rounded-lg focus:ring-slate-400 focus:ring-4 focus:outline-none bg-slate-700 text-gray-50 peer" />
+      <button id="mediaSubmit" title="Submit media" className="absolute right-2.5 bottom-2.5 flex items-center justify-center  rounded-lg bg-blue-500 peer-focus:outline-green-400 peer-focus:outline-2 text-gray-50 px-4 py-1.5 hover:ring-blue-300 hover:ring-2 hover:outline-none focus:outline-none focus:ring-blue-300 focus:ring-2"
         onClick={onMediaSubmit}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 stroke-gray-50">
+        Add media
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 ml-2 stroke-gray-50">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-
       </button>
     </div>
   )
